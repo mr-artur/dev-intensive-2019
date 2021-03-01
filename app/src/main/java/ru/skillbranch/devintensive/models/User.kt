05d5ff68d031ccba085f3dc06fa49782cb662259
Artur Morozov <ar2r.pv@gmail.com>
2021-03-01 23:32:49 +0200

    fixed User.Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -26,14 +26,16 @@ data class User(
         }
     }
 
-    data class Builder(var id: String,
-                       var firstName: String?,
-                       var lastName: String?,
-                       var avatar: String?,
-                       var rating: Int,
-                       var respect: Int,
-                       var lastVisit: Date?,
-                       var isOnline: Boolean) {
+    class Builder {
+
+        var id: String = ""
+        var firstName: String? = null
+        var lastName: String? = null
+        var avatar: String? = null
+        var rating: Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = null
+        var isOnline: Boolean = false
 
         fun id(id: String) = apply { this.id = id }
         fun firstName(firstName: String?) = apply { this.firstName = firstName }
