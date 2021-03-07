292c8961dcea252f47010e896beea59031be4f94
Artur Morozov <ar2r.pv@gmail.com>
2021-03-07 20:04:27 +0200

    fighting with tests - 1st try

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -19,7 +19,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
 
         } else {
             status = status.nextStatus()
-            "Это не правильный ответ\n${question.question}" to status.color
+            "Это неправильный ответ\n${question.question}" to status.color
         }
     }
 
